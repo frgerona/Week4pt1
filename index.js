aef78eed2cce977cc43dbaf1348424e28fed523e
frgerona <frgerona@gmail.com>
2023-01-02 07:33:09 -0800

    first commit for Simple JavaScript Function

---
+++ index.js
@@ -0,0 +1,8 @@
+var myName = 'Falah';
+var myHeight = '5ft2ins';
+var myCountry = 'USA';
+
+function myFunction(name, height, country) {
+    alert(name + ' ' + height + ' ' + country);
+    return;
+}
